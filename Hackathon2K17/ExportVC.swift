1aead84fcf899ec9dfeae10d50c2046f6562ffa6
Tal Weiss <Tweiss747@gmail.com>
2017-02-25 08:45:34 -0500

    Data passes through export reports functional

---
+++ Hackathon2K17/ExportVC.swift
@@ -0,0 +1,35 @@
+//
+//  ExportVC.swift
+//  Hackathon2K17
+//
+//  Created by Tal Weiss on 2/25/17.
+//  Copyright © 2017 Tal Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ExportVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
